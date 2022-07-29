bdb6162c6d6c8f95c1a2906f17cb647456d2f3c8
oandreazini <oriol_as_92@hotmail.com>
2022-07-29 13:53:58 +0200

    Mini Push

--- RTS-C2-TA19/src/TA19_Ex4/MainApp.java
+++ RTS-C2-TA19/src/TA19_Ex4/MainApp.java
@@ -5,7 +5,7 @@ import TA19_Ex4.GraphicApplication4;
 public class MainApp {
 
 	public static void main(String[] args) {
-		//TODO Acabar ejercicio 4
+		
 		GraphicApplication4 window = new GraphicApplication4();
 	}
 
